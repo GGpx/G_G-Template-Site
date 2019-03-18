175a54b44e41fedbf858c9ee2dd43f93285cbfe7
Jérémy Doré <j.dore@it-students.fr>
2019-03-18 09:01:55 +0100

    Modif des fichiers html css et js

--- site_mobile/main.js
+++ site_mobile/main.js
@@ -1,6 +1,7 @@
 // Fonction pour afficher le menu quand on scroll
 window.onscroll = function () {
-    scroll()
+    scroll();
+    decoration();
 };
 
 function scroll() {
@@ -11,13 +12,6 @@ function scroll() {
         // POUR LE TITRE DU HEADER
         document.getElementById('titre_header').style.color = "rgb(22, 22, 22)";
 
-        // POUR LES LIENS SUR LE HEADER
-        var tab = document.getElementsByClassName('liens_header');
-        for (i = 0; i < tab.length; i++) {
-            tab[i].style.color = "rgb(22, 22, 22)";
-            tab[0].style.textDecoration = "underline"
-        }
-
     } else {
         // POUR LE MENU HEADER
         document.getElementById('titre_header').style.color = "";
@@ -25,13 +19,122 @@ function scroll() {
         // POUR LE TITRE DU HEADER
         document.getElementById('header_menu').className = "";
 
-        // POUR LES LIENS SUR LE HEADER
+    }
+}
+
+// FONCTION POUR LA DéCORATION DES ONGLET DE LA NAVIGATION 
+
+function decoration() {
+
+    // POUR LES LIENS SUR LE HEADER / ABOUT ME
+
+    if (document.documentElement.scrollTop > 820) {
+
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+
+            // POUR LA COULEURS DE LA NAVIGATION
+            tab[i].style.color = "rgb(22, 22, 22)";
+
+            // POUR LA DéCORATION UNDERLINE
+            tab[1].style.textDecoration = "underline"
+        }
+
+    } else {
         var tab = document.getElementsByClassName('liens_header');
         for (i = 0; i < tab.length; i++) {
             tab[i].style.color = "white";
-            tab[0].style.textDecoration = "none"
+            tab[1].style.textDecoration = "none"
+        }
     }
-}
+
+
+    // POUR LES LIENS SUR LE HEADER / SERVICES
+
+    if (document.documentElement.scrollTop > 1600) {
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[1].style.textDecoration = "none"
+            tab[2].style.textDecoration = "underline"
+        }
+    } else {
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[2].style.textDecoration = "none"
+        }
+    }
+
+
+    // POUR LES LIENS SUR LE HEADER / PORTFOLIO
+    if (document.documentElement.scrollTop > 2850) {
+
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[2].style.textDecoration = "none"
+            tab[3].style.textDecoration = "underline"
+        }
+
+    } else {
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[3].style.textDecoration = "none"
+        }
+    }
+
+
+    // POUR LES LIENS SUR LE HEADER TESTIMONIALS
+    if (document.documentElement.scrollTop > 4350) {
+
+
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[3].style.textDecoration = "none"
+            tab[4].style.textDecoration = "underline"
+        }
+    } else {
+
+        // POUR LES LIENS SUR LE HEADER
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[4].style.textDecoration = "none"
+        }
+    }
+
+    // POUR LES LIENS SUR LE HEADER / OUR BLOG
+    if (document.documentElement.scrollTop > 6150) {
+
+
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[4].style.textDecoration = "none"
+            tab[5].style.textDecoration = "underline"
+        }
+    } else {
+
+        // POUR LES LIENS SUR LE HEADER
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[5].style.textDecoration = "none"
+        }
+    }
+
+    // POUR LES LIENS SUR LE HEADER / CONTACT
+    if (document.documentElement.scrollTop > 7600) {
+
+
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[5].style.textDecoration = "none"
+            tab[6].style.textDecoration = "underline"
+        }
+    } else {
+
+        // POUR LES LIENS SUR LE HEADER
+        var tab = document.getElementsByClassName('liens_header');
+        for (i = 0; i < tab.length; i++) {
+            tab[6].style.textDecoration = "none"
+        }
+    }
 }
 
 
